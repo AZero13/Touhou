7f601f73102a2dcb7d9890f690211917002e9f02
AZero13 <gfunni234@gmail.com>
2025-10-28 13:00:31 -0400

    Initial Commit

---
+++ Touhou/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Touhou
+//
+//  Created by Rose on 10/28/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
